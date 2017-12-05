75212296313b6b65144969f9cf27eb3d95f3c6b6
vcc1235 <1041497818@qq.com>
2017-12-05 15:03:31 +0800

    init

---
+++ VCRTool/VCRTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VCRTool
+//
+//  Created by alete on 2017/12/5.
+//  Copyright © 2017年 aletevcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VCRTool/VCRTool/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  VCRTool
+//
+//  Created by alete on 2017/12/5.
+//  Copyright © 2017年 aletevcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
